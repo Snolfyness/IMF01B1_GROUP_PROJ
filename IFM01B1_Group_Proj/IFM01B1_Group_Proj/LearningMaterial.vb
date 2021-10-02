d81d53d09e6a7299cbe6a377097a8602f8bb573a
Snolfyness <91134005+Snolfyness@users.noreply.github.com>
2021-10-02 13:43:03 +0200

    Options and comments

--- IFM01B1_Group_Proj/IFM01B1_Group_Proj/LearningMaterial.vb
+++ IFM01B1_Group_Proj/IFM01B1_Group_Proj/LearningMaterial.vb
@@ -1,3 +1,18 @@
-﻿Public Class LearningMaterial
+﻿' *****************************************************************
+' Team Number: assigned to team
+' Team Member 1 Details: Lin, J (221179979)
+' Team Member 2 Details: Surname, Initials (Student #)
+' Team Member 3 Details: Surname, Initials (Student #)
+' Team Member 4 Details: e.g. Smith, J (202000001)
+' Practical: Team Project
+' Class name: LearningMaterial
+' *****************************************************************
+
+Option Strict On
+Option Explicit On
+Option Infer Off
+
+
+Public Class LearningMaterial
 
 End Class
