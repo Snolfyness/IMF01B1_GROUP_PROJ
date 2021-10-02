70352f6a4ac2f6a84939080cbd6910f442b72a6c
Snolfyness <91134005+Snolfyness@users.noreply.github.com>
2021-10-02 18:59:31 +0200

    Moved Comments

--- IFM01B1_Group_Proj/IFM01B1_Group_Proj/Quiz.vb
+++ IFM01B1_Group_Proj/IFM01B1_Group_Proj/Quiz.vb
@@ -1,4 +1,13 @@
-﻿Option Strict On
+﻿' *****************************************************************
+' Team Number: assigned to team
+' Team Member 1 Details: Lin, J (221179979)
+' Team Member 2 Details: Khosa, MTS (221080579)
+' Team Member 3 Details: Rasalanavho, Initials (220054293)
+' Team Member 4 Details: none
+' Practical: Team Project
+' Class name: LearningMaterial
+' *****************************************************************
+Option Strict On
 Option Explicit On
 Option Infer Off
 
