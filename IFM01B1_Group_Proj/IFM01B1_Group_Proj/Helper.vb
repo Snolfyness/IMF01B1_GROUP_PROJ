b8ef0b73908f50b12cd0dd9c14f5fa7e1a05b3e0
Snolfyness <91134005+Snolfyness@users.noreply.github.com>
2021-10-02 18:05:50 +0200

    Started a IsInRange method Added comments

--- IFM01B1_Group_Proj/IFM01B1_Group_Proj/Helper.vb
+++ IFM01B1_Group_Proj/IFM01B1_Group_Proj/Helper.vb
@@ -2,12 +2,19 @@
 Option Explicit On
 Option Infer Off
 Public Class Helper
+    'Generate Random Number method
     Public Function GenerateRand(upper As Integer, lower As Integer) As Integer
         Dim temp As Integer
         temp = CInt((upper * Rnd()) + lower)
         Return temp
     End Function
 
+    'Checks if value is within a range
+    Public Sub IsInRange(value As String)
+        '
+    End Sub
+
+    'Checks if parameter is an integer
     Public Sub ValidateInt(value As String)
         '
     End Sub
