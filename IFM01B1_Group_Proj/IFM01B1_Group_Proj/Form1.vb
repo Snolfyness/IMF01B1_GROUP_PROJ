3dfacb30921f4b8c0eb3488da6f3e4e6a8621874
Snolfyness <91134005+Snolfyness@users.noreply.github.com>
2021-10-02 14:01:06 +0200

    Genreated Button code

--- IFM01B1_Group_Proj/IFM01B1_Group_Proj/Form1.vb
+++ IFM01B1_Group_Proj/IFM01B1_Group_Proj/Form1.vb
@@ -4,4 +4,12 @@ Option Infer Off
 
 Public Class frmELearning
     Private _Questions() As Quiz
+
+    Private Sub btnLearnMaterial_Click(sender As Object, e As EventArgs) Handles btnLearnMaterial.Click
+
+    End Sub
+
+    Private Sub btnQuiz_Click(sender As Object, e As EventArgs) Handles btnQuiz.Click
+
+    End Sub
 End Class
