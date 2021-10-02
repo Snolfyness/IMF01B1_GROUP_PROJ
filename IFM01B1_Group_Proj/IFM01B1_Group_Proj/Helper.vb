be35ae5f131df3491eb684be4eaf31903b562275
Snolfyness <91134005+Snolfyness@users.noreply.github.com>
2021-10-02 17:59:53 +0200

    Made generate randome method in helper class

--- IFM01B1_Group_Proj/IFM01B1_Group_Proj/Helper.vb
+++ IFM01B1_Group_Proj/IFM01B1_Group_Proj/Helper.vb
@@ -2,5 +2,9 @@
 Option Explicit On
 Option Infer Off
 Public Class Helper
-
+    Public Function GenerateRand(upper As Integer, lower As Integer) As Integer
+        Dim temp As Integer
+        temp = CInt((upper * Rnd()) + lower)
+        Return temp
+    End Function
 End Class
