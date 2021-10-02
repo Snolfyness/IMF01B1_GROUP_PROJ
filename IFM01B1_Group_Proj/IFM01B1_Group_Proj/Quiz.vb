b0c5047e704db32c41e5fa5c340115f28e5cb3cf
Snolfyness <91134005+Snolfyness@users.noreply.github.com>
2021-10-02 14:03:37 +0200

    Set up Quiz class constructor

--- IFM01B1_Group_Proj/IFM01B1_Group_Proj/Quiz.vb
+++ IFM01B1_Group_Proj/IFM01B1_Group_Proj/Quiz.vb
@@ -6,9 +6,16 @@ Public Class Quiz
     Private Question As String
     Private answer As Double
 
+    Public Sub New(options As Integer)
+        Select Case options
+            Case 1
+            Case 2
+            Case 3
+            Case 4
+            Case Else
+                MsgBox("Please select a valid option")
+        End Select
+    End Sub
 
-    Public Function GenerateQuestion() As String
-
-    End Function
 
 End Class
