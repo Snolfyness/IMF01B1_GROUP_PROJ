ceaa8a4d21cbdfa7f44aa1b494b05b50c30ac69a
Snolfyness <91134005+Snolfyness@users.noreply.github.com>
2021-10-05 11:05:44 +0200

    Select case 1 Maths

--- IFM01B1_Group_Proj/IFM01B1_Group_Proj/Form1.vb
+++ IFM01B1_Group_Proj/IFM01B1_Group_Proj/Form1.vb
@@ -13,48 +13,64 @@ Option Explicit On
 Option Infer Off
 
 Public Class frmELearning
+    Friend Class Helper
+    End Class
+
     Private subjects() As Subject
-    Private nsubjects As Integer
+        Private nsubjects As Integer
 
-    'this button displays the learning material of whatever subjet the learner chooses
-    Private Sub btnLearnMaterial_Click(sender As Object, e As EventArgs) Handles btnLearnMaterial.Click
-        nsubjects += 1
-        ReDim Preserve subjects(nsubjects)
+        'this button displays the learning material of whatever subjet the learner chooses
+        Private Sub btnLearnMaterial_Click(sender As Object, e As EventArgs) Handles btnLearnMaterial.Click
+            nsubjects += 1
+            ReDim Preserve subjects(nsubjects)
 
-        Dim subject As Integer = CInt(InputBox("Which subject would you like to study?" & Environment.NewLine & "Options: " & Environment.NewLine & "1 - Math" & Environment.NewLine & "2 - English" & Environment.NewLine & "3 - Natural Science"))
+            Dim subject As Integer = CInt(InputBox("Which subject would you like to study?" & Environment.NewLine & "Options: " & Environment.NewLine & "1 - Math" & Environment.NewLine & "2 - English" & Environment.NewLine & "3 - Natural Science"))
 
-        Dim mark As Integer
+            Dim mark As Integer
 
-        'Choices for the learning material
+            'Choices for the learning material
 
-        Select Case subject
-            Case 1 ' mathematics
-                txtdisplay.Clear()
-                Dim chapter As Integer = CInt(InputBox("Which chapter would you like to focus on?" & Environment.NewLine & "Options:" & Environment.NewLine & "1 - Addition" & Environment.NewLine & "2 - Subtraction" & Environment.NewLine & "3 - Multiplication" & Environment.NewLine & "4 - Division"))
-                Dim maths As math
-                maths = New math(subject, mark, chapter)
-                subjects(nsubjects) = maths
-                txtdisplay.Text &= maths.displayLearningmaterial()
+            Select Case subject
+                Case 1 ' mathematics
+                    txtdisplay.Clear()
+                    Dim chapter As Integer = CInt(InputBox("Which chapter would you like to focus on?" & Environment.NewLine & "Options:" & Environment.NewLine & "1 - Addition" & Environment.NewLine & "2 - Subtraction" & Environment.NewLine & "3 - Multiplication" & Environment.NewLine & "4 - Division"))
+                    Dim maths As math
+                    maths = New math(subject, mark, chapter)
+                    subjects(nsubjects) = maths
+                    txtdisplay.Text &= maths.displayLearningmaterial()
 
-            Case 2 'english
-                txtdisplay.Clear()
-                Dim chapter As Integer = CInt(InputBox("Which chapter would you like to focus on?" & Environment.NewLine & "Options:" & Environment.NewLine & "1 - Literature" & Environment.NewLine & "2 - Poems" & Environment.NewLine & "3 - " & Environment.NewLine & "4 - Division"))
-
-
-        End Select
-
-        'this is polymorphism
+                Case 2 'english
+                    txtdisplay.Clear()
+                    Dim chapter As Integer = CInt(InputBox("Which chapter would you like to focus on?" & Environment.NewLine & "Options:" & Environment.NewLine & "1 - Literature" & Environment.NewLine & "2 - Poems" & Environment.NewLine & "3 - " & Environment.NewLine & "4 - Division"))
 
 
-    End Sub
+            End Select
 
-    Private Sub btnQuiz_Click(sender As Object, e As EventArgs) Handles btnQuiz.Click
-        Dim choice As Integer
-        Select Case choice
-            Case 1 'Maths
-            Case 2 'English
-            Case Else
-                txtdisplay.Text = "Please enter a valid option" & Environment.NewLine
-        End Select
-    End Sub
-End Class
+            'this is polymorphism
+
+
+        End Sub
+
+        Private Sub btnQuiz_Click(sender As Object, e As EventArgs) Handles btnQuiz.Click
+            Dim choice As Integer
+            Select Case choice
+                Case 1 'Maths
+                    Dim Quiz(10) As MathQuestion
+                    For q As Integer = 1 To 10
+                        Dim temp As String
+                        Dim question As Integer
+                        question = q Mod 5
+
+                        Select Case question
+                            Case 0
+
+                        End Select
+
+                        temp = InputBox("What is ")
+                    Next
+                Case 2 'English
+                Case Else
+                    txtdisplay.Text = "Please enter a valid option" & Environment.NewLine
+            End Select
+        End Sub
+    End Class
