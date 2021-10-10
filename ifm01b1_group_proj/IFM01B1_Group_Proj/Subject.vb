97a4535f1d3663d73e03133fa70eec991d9f7d41
Snolfyness <91134005+Snolfyness@users.noreply.github.com>
2021-10-10 20:18:59 +0200

    Fixed validate function in sunject class

---
+++ ifm01b1_group_proj/IFM01B1_Group_Proj/Subject.vb
@@ -0,0 +1,86 @@
+﻿
+' *****************************************************************
+' Team Number: assigned to team
+' Team Member 1 Details: Lin, J (221179979)
+' Team Member 2 Details: Khosa, MTS (221080579)
+' Team Member 3 Details: Rasalanavho, Initials (220054293)
+' Team Member 4 Details: none
+' Practical: Team Project
+' Class name: LearningMaterial
+' *****************************************************************
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+'this is the abstract or base class
+<Serializable()> Public MustInherit Class Subject
+
+    'these are the attributes
+    Private _name As Integer
+    Private _mark As Double
+    Private _total As Double
+    Private _average As Double
+
+
+    'these are the property methods
+    Public Property name As Integer
+        Get
+            Return _name
+        End Get
+        Set(value As Integer)
+            _name = validate(name)
+        End Set
+    End Property
+
+    Public Property mark As Double
+        Get
+            Return _mark
+        End Get
+        Set(value As Double)
+            _mark = validate(value)
+        End Set
+    End Property
+
+    Public Property total As Double
+        Get
+            Return _total
+        End Get
+        Set(value As Double)
+            _total = validate(value)
+        End Set
+    End Property
+
+    Public Property average As Double
+        Get
+            Return _average
+
+        End Get
+        Set(value As Double)
+            _average = validate(value)
+        End Set
+    End Property
+
+    'END OF PROPERTY METHODS____________________________________________
+
+
+    'this function generates the learning materials that are needed for the quizes
+    Public MustOverride Function displayLearningmaterial() As String
+
+
+    'this is the constructor
+    Public Sub New(name As Integer, mark As Double)
+        _name = name
+        Me.mark = mark
+    End Sub
+
+    'this function validates all integer variable
+    Protected Function validate(value As Double) As Integer
+        If value < 0 Then
+            Return 0
+        Else
+            Return CInt(value)
+        End If
+    End Function
+
+End Class
